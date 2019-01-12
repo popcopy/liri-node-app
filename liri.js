1f0082ed0fdfe075f8dc4b0c5992ec8fe002927f
Matt Urian <matt.a.urian@gmail.com>
2019-01-11 19:18:23 -0500

    adding package-lock

--- liri.js
+++ liri.js
@@ -1,2 +1,15 @@
-to read and set any environment variables with the dotenv package:
+// to read and set any environment variables with the dotenv package:
 require("dotenv").config();
+
+//Grab data from keys.js -- npm require
+var spotify = new Spotify(keys.spotify);
+var fs = require('fs');
+
+//Stored argument's array
+var input = process.argv;
+var command = process.argv[2];
+
+
+
+
+
